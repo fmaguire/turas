7288a0655438e48d1617cf02fdfa708fa71ffcbb
Finlay Maguire <finlaymaguire@gmail.com>
2016-06-06 04:15:03 +0100

    add simple view for landing index

--- turas/__init__.py
+++ turas/__init__.py
@@ -1,4 +1,8 @@
-from flask import Flask
+#!/usr/bin/env python3
 
+from flask import Flask
 app = Flask(__name__)
+
 app.config.from_object('config')
+
+import turas.views
---
+++ turas/models.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import googlemaps
+
+"""
+# Replace the API key below with a valid API key.
+gmaps = googlemaps.Client(key='YOUR_API_KEY')
+
+# Geocoding and address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+
+"""
